a1937281a11e0277cf13fd69dbf5e225442eff90
barretot <barretoruan77@gmail.com>
2021-04-14 13:33:32 -0300

    maquina de escrever ok

---
+++ public/scripts/nav-bar.js
@@ -0,0 +1,20 @@
+window.onhashchange = (event) => {
+  const oldURL = event.oldURL.split('#')[1];
+  const newURL = event.newURL.split('#')[1];
+  console.log(oldURL, newURL);
+  const oldMenu = document.querySelector(`nav a[href="#${oldURL}"]`);
+  const newMenu = document.querySelector(`nav a[href="#${newURL}"]`);
+  console.log(oldMenu, newMenu);
+  oldMenu && oldMenu.classList.remove('selected');
+  newMenu && newMenu.classList.add('selected');
+};
+
+function typeWritter(texto, tempo) {
+  let char = texto.split('').reverse();
+  let typer = setInterval(function () {
+    if (!char.length) return clearInterval(typer);
+    let next = char.pop();
+    document.querySelector('#text').innerHTML += next;
+  }, 200);
+}
+typeWritter('Desenvolvedor Web', 'log', 100);
